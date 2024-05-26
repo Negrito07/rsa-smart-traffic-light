c58e89c93072a5f28d5d03fb8b1ba39e2e358f1f
Gabriel Negri <negrigabriel@hotmail.com>
2024-05-26 16:43:29 +0100

    Added app init

--- main/api.py
+++ main/api.py
@@ -3,34 +3,43 @@ import paho.mqtt.client as mqtt
 import threading
 
 class Monitor:
-    '''Helper class to listen to a given vanetza
+    '''Helper class to listen to a given
     mqtt topic published by a given host.'''
 
-    def __init__(self, host, topic):
+    def __init__(self, host, topic, on_message=lambda msg: msg):
         self.host = host
         self.topic = topic
+        self.on_message = on_message
 
-    def on_connect(self, client, obj, flags, rc):
+    def handle_connect(self, client, obj, flags, rc):
         print("Connected with result code "+str(rc))
-        client.subscribe(f"vanetza/in/{self.topic}")
+        client.subscribe(self.topic)
 
-    def on_message(self, client, obj, msg):
+    def handle_message(self, client, obj, msg):
         message = msg.payload.decode('utf-8')
+        topic = msg.topic
         print('Topic: ' + msg.topic)
         print('Message: ' + message)
-        obj = json.loads(message)
+        self.on_message(topic, message)
+
+    def start(self):
+        client = mqtt.Client()
+        client.on_connect = self.handle_connect
+        client.on_message = self.handle_message
+        client.connect(self.host, 1883, 60)
+        threading.Thread(target=client.loop_forever).start()
 
 
 # hosts/topics to listen
-monitors = [
-    Monitor("192.168.98.10", "spatem"),     # rsu
-    Monitor("192.168.98.20", "cam")         # obu
-]
+# monitors = [
+#     Monitor("192.168.98.10", "spatem"),     # rsu
+#     Monitor("192.168.98.20", "cam")         # obu
+# ]
 
-for monitor in monitors:
-    client = mqtt.Client()
-    client.on_connect = monitor.on_connect
-    client.on_message = monitor.on_message
-    client.connect(monitor.host, 1883, 60)
-    threading.Thread(target=client.loop_forever).start()
+# for monitor in monitors:
+#     client = mqtt.Client()
+#     client.on_connect = monitor.on_connect
+#     client.on_message = monitor.on_message
+#     client.connect(monitor.host, 1883, 60)
+#     threading.Thread(target=client.loop_forever).start()
     
---
+++ main/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from api import Monitor
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route("/")
+def root():
+    return render_template("index.html")
+
+# hosts/topics to listen
+monitors = [
+    Monitor("192.168.98.10", "vanetza/in/spatem"),     # rsu
+    Monitor("192.168.98.20", "vanetza/in/cam")         # obu
+]
+
+@socketio.on('connect')
+def start():
+    print('Client connected.')
+    print('Starting monitors...')
+    for monitor in monitors:
+        monitor.on_message = lambda topic, msg: socketio.emit(topic, msg)
+        monitor.start()
+        print(f"{monitor.topic} monitor: OK!")
+    socketio.emit('connect', {'data': 'Connected to monitors'})
+
+@socketio.on('disconnect')
+def end():
+    print('Disonnected')
+    socketio.emit('disconnect', {'data': 'Disconnected'})
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
